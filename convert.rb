22d60aa903121678d7515c824e65fd33c6150042
Justin Richter <jrichter@jetfive.com>
2015-10-20 18:56:21 -0500

    Expand to class, understand what next does

--- convert.rb
+++ convert.rb
@@ -1,93 +1,110 @@
 # -*- coding: utf-8 -*-
 require ('builder')
 
-BOOKS = {
-  'Gn' => 'Genesis',
-  'Ex' => 'Exodus',
-  'Lv' => 'Leviticus',
-  'Nm' => 'Numeri',
-  'Dt' => 'Deuteronomium',
-  'Jos' => 'Josue',
-  'Jdc' => 'Judicum',
-  'Rt' => 'Ruth',
-  '1Rg' => 'Regum I',
-  '2Rg' => 'Regum II',
-  '3Rg' => 'Regum III',
-  '4Rg' => 'Regum IV',
-  '1Par' => 'Paralipomenon I',
-  '2Par' => 'Paralipomenon II',
-  'Esr' => 'Esdræ',
-  'Neh' => 'Nehemiæ',
-  'Tob' => 'Tobiæ',
-  'Jdt' => 'Judith',
-  'Est' => 'Esther',
-  'Job' => 'Job',
-  'Ps' => 'Psalmi',
-  'Pr' => 'Proverbia',
-  'Ecl' => 'Ecclesiastes',
-  'Ct' => 'Canticum Canticorum',
-  'Sap' => 'Sapientia',
-  'Sir' => 'Ecclesiasticus',
-  'Is' => 'Isaias',
-  'Jr' => 'Jeremias',
-  'Lam' => 'Lamentationes',
-  'Bar' => 'Baruch',
-  'Ez' => 'Ezechiel',
-  'Dn' => 'Daniel',
-  'Os' => 'Osee',
-  'Joel' => 'Joël',
-  'Am' => 'Amos',
-  'Abd' => 'Abdias',
-  'Jon' => 'Jonas',
-  'Mch' => 'Michæa',
-  'Nah' => 'Nahum',
-  'Hab' => 'Habacuc',
-  'Soph' => 'Sophonias',
-  'Agg' => 'Aggæus',
-  'Zach' => 'Zacharias',
-  'Mal' => 'Malachias',
-  '1Mcc' => 'Machabæorum I',
-  '2Mcc' => 'Machabæorum II',
-  'Mt' => 'Matthæus',
-  'Mc' => 'Marcus',
-  'Lc' => 'Lucas',
-  'Jo' => 'Joannes',
-  'Act' => 'Actus Apostolorum',
-  'Rom' => 'ad Romanos',
-  '1Cor' => 'ad Corinthios I',
-  '2Cor' => 'ad Corinthios II',
-  'Gal' => 'ad Galatas',
-  'Eph' => 'ad Ephesios',
-  'Phlp' => 'ad Philippenses',
-  'Col' => 'ad Colossenses',
-  '1Thes' => 'ad Thessalonicenses I',
-  '2Thes' => 'ad Thessalonicenses II',
-  '1Tim' => 'ad Timotheum I',
-  '2Tim' => 'ad Timotheum II',
-  'Tit' => 'ad Titum',
-  'Phlm' => 'ad Philemonem',
-  'Hbr' => 'ad Hebræos',
-  'Jac' => 'Jacobi',
-  '1Ptr' => 'Petri I',
-  '2Ptr' => 'Petri II',
-  '1Jo' => 'Joannis I',
-  '2Jo' => 'Joannis II',
-  '3Jo' => 'Joannis III',
-  'Jud' => 'Judæ',
-  'Apc' => 'Apocalypsis'
-}
+class LatinConverter
 
-builder = Builder::XmlMarkup.new
-builder.instruct!(:xml, version: '1.0', encoding: 'UTF-8')
+  BOOKS = {
+           'Gn'    => ['GEN','Genesis'],
+           'Ex'    => ['EXO','Exodus'],
+           'Lv'    => ['LEV','Leviticus'],
+           'Nm'    => ['NUM','Numeri'],
+           'Dt'    => ['DEU','Deuteronomium'],
+           'Jos'   => ['JOS','Josue'],
+           'Jdc'   => ['JDG','Judicum'],
+           'Rt'    => ['RUT','Ruth'],
+           '1Rg'   => ['1SA','Regum I'],
+           '2Rg'   => ['2SA','Regum II'],
+           '3Rg'   => ['1KI','Regum III'],
+           '4Rg'   => ['2KI','Regum IV'],
+           '1Par'  => ['1CH','Paralipomenon I'],
+           '2Par'  => ['2CH','Paralipomenon II'],
+           'Esr'   => ['EZR','Esdræ'],
+           'Neh'   => ['NEH','Nehemiæ'],
+           'Tob'   => ['TOB','Tobiæ'],
+           'Jdt'   => ['JDT','Judith'],
+           'Est'   => ['EST','Esther'],
+           'Job'   => ['JOB','Job'],
+           'Ps'    => ['PSA','Psalmi'],
+           'Pr'    => ['PRO','Proverbia'],
+           'Ecl'   => ['ECC','Ecclesiastes'],
+           'Ct'    => ['SNG','Canticum Canticorum'],
+           'Sap'   => ['WIS','Sapientia'],
+           'Sir'   => ['SIR','Ecclesiasticus'],
+           'Is'    => ['ISA','Isaias'],
+           'Jr'    => ['JER','Jeremias'],
+           'Lam'   => ['LAM','Lamentationes'],
+           'Bar'   => ['BAR','Baruch'],
+           'Ez'    => ['EZK','Ezechiel'],
+           'Dn'    => ['DAN','Daniel'],
+           'Os'    => ['HOS','Osee'],
+           'Joel'  => ['JOL','Joël'],
+           'Am'    => ['AMO','Amos'],
+           'Abd'   => ['OBA','Abdias'],
+           'Jon'   => ['JON','Jonas'],
+           'Mch'   => ['MIC','Michæa'],
+           'Nah'   => ['NAM','Nahum'],
+           'Hab'   => ['HAB','Habacuc'],
+           'Soph'  => ['ZEP','Sophonias'],
+           'Agg'   => ['HAG','Aggæus'],
+           'Zach'  => ['ZEC','Zacharias'],
+           'Mal'   => ['MAL','Malachias'],
+           '1Mcc'  => ['1MA','Machabæorum I'],
+           '2Mcc'  => ['2MA','Machabæorum II'],
+           'Mt'    => ['MAT','Matthæus'],
+           'Mc'    => ['MRK','Marcus'],
+           'Lc'    => ['LUK','Lucas'],
+           'Jo'    => ['JHN','Joannes'],
+           'Act'   => ['ACT','Actus Apostolorum'],
+           'Rom'   => ['ROM','ad Romanos'],
+           '1Cor'  => ['1CO','ad Corinthios I'],
+           '2Cor'  => ['2CO','ad Corinthios II'],
+           'Gal'   => ['GAL','ad Galatas'],
+           'Eph'   => ['EPH','ad Ephesios'],
+           'Phlp'  => ['PHP','ad Philippenses'],
+           'Col'   => ['COL','ad Colossenses'],
+           '1Thes' => ['1TH','ad Thessalonicenses I'],
+           '2Thes' => ['2TH','ad Thessalonicenses II'],
+           '1Tim'  => ['1TI','ad Timotheum I'],
+           '2Tim'  => ['2TI','ad Timotheum II'],
+           'Tit'   => ['TIT','ad Titum'],
+           'Phlm'  => ['PHM','ad Philemonem'],
+           'Hbr'   => ['HEB','ad Hebræos'],
+           'Jac'   => ['JAS','Jacobi'],
+           '1Ptr'  => ['1PE','Petri I'],
+           '2Ptr'  => ['2PE','Petri II'],
+           '1Jo'   => ['1JN','Joannis I'],
+           '2Jo'   => ['2JN','Joannis II'],
+           '3Jo'   => ['3JN','Joannis III'],
+           'Jud'   => ['JUD','Judæ'],
+           'Apc'   => ['REV','Apocalypsis']
+          }
 
-Dir['latin/*.lat'].to_a.sort.each do |filename|
-  next if filename =~~ /.*\.lat/
-  file = File.open(filename, 'r')
-  book = Books[File.basename(filename, '.lat')]
-  file.readlines.each do |line|
-    # The original file has codepage 1252 encoding
-    # Lets get rid of that
-    line.force_encoding('windows-1252').encode!('utf-8')
-
+  def self.build_verses_hash
+    verses = {}
+    Dir['latin/*.lat'].to_a.sort.each do |filename|
+      if filename =~ /.*\.lat/
+        file = File.open(filename, 'r')
+        name = File.basename(filename, '.lat')
+        puts name
+        bid = BOOKS[name][0]
+        verses[bid] ||= {}
+        file.readlines.each do |line|
+          # The original file has codepage 1252 encoding
+          # Lets get rid of that
+          line.force_encoding('windows-1252').encode!('utf-8')
+          # Get chapter and verse number
+          cn, vn = line.slice!(/\d+:\d+/).split(':')
+          verses[bid][cn] ||= {}
+          verses[bid][cn][vn] = line.strip
+        end
+      end
+      return verses
+    end
   end
+
+  def self.build_xml(verses)
+    builder = Builder::XmlMarkup.new
+    builder.instruct!(:xml, version: '1.0', encoding: 'UTF-8')
+  end
+
 end
